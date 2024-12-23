8ef0f6cd45353a04f8594d220863f65a6f310f72
Vinicius dos Santos <143943952+viniciusdossantosss@users.noreply.github.com>
2024-12-23 14:47:59 +0000

    Adiciona `link:check e `link:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Eu vou concluir esse curso!</h1>
+  return <h1>Eu vou concluir esse curso!</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
